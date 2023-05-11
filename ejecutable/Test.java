c359493b479ab550e1688fff9d1ef151b5156ffe
Alvoid101 <10101010alvoid@gmail.com>
2023-05-11 08:01:53 -0500

    accesibilidad atributos Fecha

---
+++ ejecutable/Test.java
@@ -0,0 +1,13 @@
+package ejecutable;
+
+import modelo.Fecha;
+
+public class Test
+{
+    public static void main(String[] args) {
+       Fecha f = new Fecha();
+       f.dia = 11;
+       f.mes = 5;
+       f.anio = 2023; 
+    }
+}
---
+++ modelo/Fecha.java
@@ -0,0 +1,11 @@
+package modelo;
+public class Fecha
+{
+    //---Atributos---
+    private int dia;
+    private int mes;
+    private int anio;
+
+    
+
+}
